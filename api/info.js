70a3a6a42ba4d08d7823ffd9370629ccd3c21263
EduFilesPH <edufilesph@gmail.com>
2026-09-18 11:28:53 +0800

    fix: use TV embedded client for YouTube playback

--- api/info.js
+++ api/info.js
@@ -85,7 +85,7 @@ export default async function handler(req, res) {
 
   try {
     const youtube = await getYouTube();
-    const info = await youtube.getInfo(videoId);
+    const info = await youtube.getInfo(videoId, { client: 'TV_EMBEDDED' });
     const basic = info.basic_info || {};
 
     if (basic.is_private) {
